09fb6c3d9d4bc46d0550b24287f67a61fde8b593
cbetori <cbetori@gmail.com>
2022-03-24 12:25:11 -0500

    proxy

--- webpack.config.js
+++ webpack.config.js
@@ -3,72 +3,77 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const deps = require('./package.json').dependencies
 
-module.exports = (_, argv) => {
-  console.log(argv)
-  return {
-    entry: './src/index.js',
-    mode: 'development',
-    devtool: 'hidden-source-map',
-    resolve: {
-      extensions: ['.js', '.json', '.css', '.png'],
-    },
-    module: {
-      rules: [
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'hidden-source-map',
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.png'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'consumer',
+      filename: 'remoteEntry.js',
+      remotes: {
+        resume: prod
+          ? 'resume@https://svelte-resume-phi.vercel.app/remoteEntry.js'
+          : 'resume@http://localhost:3000/remoteEntry.js',
+
+        pokemon: prod
+          ? 'pokemon@https://pokemon-battle-cb.herokuapp.com/remoteEntry.js'
+          : 'pokemon@http://localhost:3001/remoteEntry.js',
+
+        covid: prod
+          ? 'covid@https://sars-cov-2-cb.herokuapp.com/remoteEntry.js'
+          : 'covid@http://localhost:3002/remoteEntry.js',
+      },
+      shared: [
         {
-          test: /\.js$/,
-          include: path.resolve(__dirname, 'src'),
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
           },
-        },
-        {
-          test: /\.(png)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          'react-dom': {
+            singleton: true,
+            requiredVersion: deps['react-dom'],
+          },
         },
       ],
+    }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    proxy: {
+      '/api': prod
+        ? 'https://sars-cov-2-cb.herokuapp.com/'
+        : 'http://localhost:3002/',
     },
-
-    plugins: [
-      new ModuleFederationPlugin({
-        name: 'consumer',
-        filename: 'remoteEntry.js',
-        remotes: {
-          resume:
-            argv.mode === 'development'
-              ? 'resume@http://localhost:3000/remoteEntry.js'
-              : 'resume@https://svelte-resume-phi.vercel.app/remoteEntry.js',
-          pokemon:
-            argv.mode === 'development'
-              ? 'pokemon@http://localhost:3000/remoteEntry.js'
-              : 'pokemon@https://pokemon-battle-cb.herokuapp.com/remoteEntry.js',
-
-          covid:
-            argv.mode === 'development'
-              ? 'covid@http://localhost:3003/remoteEntry.js'
-              : 'covid@https://sars-cov-2-cb.herokuapp.com/remoteEntry.js',
-        },
-        shared: [
-          {
-            ...deps,
-            react: {
-              singleton: true,
-              requiredVersion: deps.react,
-            },
-            'react-dom': {
-              singleton: true,
-              requiredVersion: deps['react-dom'],
-            },
-          },
-        ],
-      }),
-      new HtmlWebPackPlugin({
-        template: './public/index.html',
-      }),
-    ],
-  }
+  },
 }
